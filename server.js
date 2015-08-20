0345a476da22e391cb30a5318008e336a9ab8c1f
Han Koh <rhgksrua@gmail.com>
2015-08-20 15:36:58 -0500

    test server

---
+++ server.js
@@ -0,0 +1,16 @@
+// Porfolio
+
+
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+var ip = process.env.NODE_ENV === 'production' ? '45.55.42.72' : '127.0.0.1';
+
+app.get('/', function(req, res) {
+    res.send('hello');
+});
+app.listen(port, function() {
+    console.log('http://' + ip + ':' + port);
+});
