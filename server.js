2e923bc93472c170ad77469d4a10adbf08668957
Han Koh <rhgksrua@gmail.com>
2015-08-20 16:28:40 -0500

    final test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var port = process.env.PORT || 3000;
 var ip = process.env.NODE_ENV === 'production' ? '45.55.42.72' : '127.0.0.1';
 
 app.get('/', function(req, res) {
-    res.send('hello world');
+    res.send('hello world yay!');
 });
 app.listen(port, function() {
     console.log('http://' + ip + ':' + port);
